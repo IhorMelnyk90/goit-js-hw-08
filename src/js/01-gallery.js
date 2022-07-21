284947943a7d8c7558718f09e15a97b965ec4ec6
Ігор <igor.melnyk2011@gmail.com>
2022-07-21 21:22:54 +0300

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryImages = document.querySelector('.gallery');
+
+const createMarkup = createGalleryImage(galleryItems);
+
+galleryImages.insertAdjacentHTML('beforeend', createMarkup);
+
+function createGalleryImage(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    }).join('');
+}
+
+new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
