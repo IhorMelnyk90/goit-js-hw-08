e6086424b7df18a4c33da80185ec7e199c2118e0
Ігор <igor.melnyk2011@gmail.com>
2022-07-24 19:32:44 +0300

    task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+
+const STORAGE_KEY = "feedback-form-state";
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', formSubmit);
+
+populateForm();
+
+const formData = {};
+
+function onInput (event){
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function formSubmit(event){
+    event.preventDefault();
+    console.log('email ',inputEl.value)
+    console.log('message', messageEl.value);    
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateForm () {    
+    const newForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+ 
+ if (newForm) {
+    const savedInput = newForm.email;
+    inputEl.value = savedInput;
+    const savedMessage = newForm.message;
+    messageEl.value = savedMessage;
+ };
+};
