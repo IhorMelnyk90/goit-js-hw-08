d45c5053f5f385b3314a07441674352897ebdcd4
Ігор <igor.melnyk2011@gmail.com>
2022-07-24 17:39:17 +0300

    task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function(data) {
+    const time = data.seconds;
+    localStorage.setItem("videoplayer-current-time", time);
+};
+const saveTime = localStorage.getItem("videoplayer-current-time");
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (saveTime){
+    player.setCurrentTime(saveTime)
+}
